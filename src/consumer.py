8a5e31497d0d82f24c0da0d2ba4b006fce2fae7f
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2025-01-16 11:05:40 +0900

    Update consumer.py

--- src/consumer.py
+++ src/consumer.py
@@ -1,1 +1,20 @@
+from kafka import KafkaConsumer
+from json import loads
 
+consumer = KafkaConsumer(
+    'topic2', # 토픽명
+    bootstrap_servers=['localhost:9092'], 
+    auto_offset_reset='earliest', 
+    enable_auto_commit=True, # 오프셋 자동 커밋 여부
+  #  group_id='test-group', # 컨슈머 그룹 식별자
+    key_deserializer=lambda x: loads(x.decode('utf-8')), 
+    value_deserializer=lambda x: loads(x.decode('utf-8')),
+    consumer_timeout_ms=1000 
+)
+
+print('[Start] get consumer')
+
+for message in consumer:
+    print(f'Topic : {message.topic}, Partition : {message.partition}, Offset : {message.offset}, Key : {message.key}, value : {message.value}')
+
+print('[End] get consumer')
